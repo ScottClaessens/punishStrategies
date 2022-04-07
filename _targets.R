6de0471bdf8afe9dbbc06215e2ff248f6de915d1
ScottClaessens <scott.claessens@gmail.com>
2022-04-07 17:29:10 +1200

    first commit

---
+++ _targets.R
@@ -0,0 +1,11 @@
+library(targets)
+library(tarchetypes)
+library(tidyverse)
+source("R/functions.R")
+options(tidyverse.quiet = TRUE)
+#tar_option_set(packages = c())
+
+# full workflow
+list(
+  
+)
